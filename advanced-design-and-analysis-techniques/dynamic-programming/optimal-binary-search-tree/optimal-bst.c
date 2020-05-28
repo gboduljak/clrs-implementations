28e1e34317b50e71db30d45d9952d3205bfa4ff6
Gabrijel Boduljak <gboduljak@users.noreply.github.com>
2020-05-29 00:27:44 +0200

    Removed some comments from the optimal bst code
    removed comments

--- advanced-design-and-analysis-techniques/dynamic-programming/optimal-binary-search-tree/optimal-bst.c
+++ advanced-design-and-analysis-techniques/dynamic-programming/optimal-binary-search-tree/optimal-bst.c
@@ -45,7 +45,6 @@ optimal_bst_result OptimalBst(double *p, double *q, int n) {
 char *concat(const char *s1, const char *s2) {
   char *result =
       malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
-  // in real code you would check for errors in malloc here
   strcpy(result, s1);
   strcat(result, s2);
   return result;
