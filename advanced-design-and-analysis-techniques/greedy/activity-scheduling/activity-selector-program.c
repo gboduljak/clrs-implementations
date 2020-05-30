5a2836de62c2421ece79d7cfd3dad3a8e6becfec
Gabrijel Boduljak <gboduljak@outlook.com>
2020-05-30 23:37:21 +0200

    implemented greedy activity scheduler

---
+++ advanced-design-and-analysis-techniques/greedy/activity-scheduling/activity-selector-program.c
@@ -0,0 +1,27 @@
+#include "activity-selector.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+int main()
+{
+  int n, i;
+  scanf("%d", &n);
+
+  int *s = (int *)malloc(sizeof(int) * n);
+  int *f = (int *)malloc(sizeof(int) * n);
+
+  for (i = 0; i < n; i++)
+    scanf("%d", &s[i]);
+  for (i = 0; i < n; i++)
+    scanf("%d", &f[i]);
+
+  // assumes the activities are sorted by finishing time in nondecreasing order
+  linked_list *schedule = AllocateLinkedList();
+  schedule = RecursiveActivitySelector(schedule, s, f, -1, n);
+
+  PrintSchedule(schedule, schedule->nil->next);
+  DellocateLinkedList(schedule);
+  free(s);
+  free(f);
+  return 0;
+}
---
+++ advanced-design-and-analysis-techniques/greedy/activity-scheduling/activity-selector.c
@@ -0,0 +1,35 @@
+#include "activity-selector.h"
+#include "stdio.h"
+
+linked_list *RecursiveActivitySelector(linked_list *scheduled, int *s, int *f, int k, int n)
+{
+  int m = k + 1;
+  while (s[m] < f[k])
+    m++;
+  if (m >= n)
+    return scheduled;
+  ListInsert(scheduled, m);
+  return RecursiveActivitySelector(scheduled, s, f, m, n);
+}
+
+linked_list *GreedyActivitySelector(int *s, int *f, int n)
+{
+  linked_list *result = AllocateLinkedList();
+  ListInsert(result, 0);
+  int k = 0, m;
+
+  for (m = 1; m < n; m++)
+    if (s[m] >= f[k])
+      ListInsert(result, m), k = m;
+
+  return result;
+}
+
+void PrintSchedule(linked_list *list, linked_list_node *current)
+{
+  if (current == list->nil)
+    return;
+
+  PrintSchedule(list, current->next);
+  printf("perform activity: %d \n", current->key);
+}
